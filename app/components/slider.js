54fe743e8f8753afda5bb71b40ce1ef26caddc2b
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-06 02:11:22 -0400

    chore: Refactor slider component to use section instead of ul

--- app/components/slider.js
+++ app/components/slider.js
@@ -26,7 +26,7 @@ export function slider({ el, children }) {
       <div class='glide__track w-full h-full' data-glide-el='track'>
         <section class='glide__slides w-full h-full'>
           ${children()}
-        </ul>
+        </section>
       </div>
       <div class='glide__arrows opacity-0 w-full flex flex-row justify-between' data-glide-el='controls'>
         <button class='glide__arrow glide__arrow--left w-1/3 h-full' data-glide-dir='<'>prev</button>
