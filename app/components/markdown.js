a137649f730f347f9ef55f8101c937f84aec3de1
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-07 22:36:33 -0400

    chore: Update meta charset and add content fallback for Markdown component

--- app/components/markdown.js
+++ app/components/markdown.js
@@ -8,7 +8,7 @@ export function markdown({ el, children, props }) {
 
   el.className = `flex flex-col justify-center items-center w-full h-full p-0 m-0 prose prose-slate dark:prose-invert max-w-none ${className}`
 
-  return children
+  return () => children()
 }
 
 load(markdown, 'Markdown')
--- app/index.js
+++ app/index.js
@@ -2,6 +2,14 @@ import { html, load, Emmy } from 'emmy-dom/dist/server'
 import './components/slider'
 import './components/counter'
 
+const contentFallback = `
+  <section class='w-full h-full p-4 text-xl bg-yellow-500 dark:bg-yellow-700'>
+    No se ha encontrado contenido.
+    Esto puede deberse a que la página no ha cargado correctamente.
+    Comprueba tener JavaScript habilitado.
+  </section>
+`
+
 export function app({ el }) {
   el.className = 'flex flex-col justify-center items-center text-center w-full h-full'
 
@@ -13,23 +21,23 @@ export function app({ el }) {
       </Slide>
       <Slide classname='md:!flex-row'>
         <Markdown classname='lg:!w-2/5'>
-          ${Emmy.markdown.lit1}
+          ${Emmy.markdown.lit1 ?? contentFallback}
         </Markdown>
         <Markdown classname='lg:!w-3/5'>
-          ${Emmy.markdown.lit2}
+          ${Emmy.markdown.lit2 ?? contentFallback}
         </Markdown>
       </Slide>
       <Slide>
         <Markdown classname='lg:!w-1/2'>
-          ${Emmy.markdown.emmy1}
+          ${Emmy.markdown.emmy1 ?? contentFallback}
         </Markdown>
       </Slide>
       <Slide classname='md:!flex-row'>
         <Markdown classname='lg:!w-2/6'>
-          ${Emmy.markdown.emmy2}
+          ${Emmy.markdown.emmy2 ?? contentFallback}
         </Markdown>
         <Markdown classname='lg:!w-4/6'>
-          ${Emmy.markdown.emmy3}
+          ${Emmy.markdown.emmy3 ?? contentFallback}
         </Markdown>
       </Slide>
       <Slide classname='h-fit'>
@@ -37,7 +45,7 @@ export function app({ el }) {
       </Slide>
       <Slide>
         <Markdown classname='lg:!w-3/4'>
-          ${Emmy.markdown.emmy4}
+          ${Emmy.markdown.emmy4 ?? contentFallback}
         </Markdown>
       </Slide>
     </Slider>
